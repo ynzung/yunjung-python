86fbf407b30f617534bede58e381b75964a564f3
SEONG YUNJUNG <syj91105@sookmyung.ac.kr>
2025-08-03 08:59:44 +0900

    Update A＋B － 4.py

--- Python/백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ Python/백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,11 @@
+# 런타임 에러 발생, 입력 함수 호출 시 적절한 입력 제공되는지 확인하고 
+# 파일의 끝을 예외처리 해야함 
+# 참고 블로그: https://machinelog.tistory.com/entry/Python-%EB%9F%B0%ED%83%80%EC%9E%84-%EC%97%90%EB%9F%AC
+
 while True:
     try:
         n, m = map(int, input().split())
         print(n + m)
     except EOFError:
+
         break

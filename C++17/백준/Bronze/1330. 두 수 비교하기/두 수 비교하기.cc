6d6be5ec4d740153a77b41062537904c48ad90c9
SEONG YUNJUNG <syj91105@sookmyung.ac.kr>
2025-08-03 17:04:08 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long A, B;
+    
+    cin >> A >> B;
+    
+    if (A > B) {
+        cout << ">" << endl;
+    }
+    else if (A < B) {
+        cout << "<" << endl;
+    }
+    else {
+        cout  << "==" << endl;
+    }
+    
+    return 0;
+}

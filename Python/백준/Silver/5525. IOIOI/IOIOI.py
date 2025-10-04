4bd977a11a8cf7d7adc988cdc43db5832177ff54
Seong Yunjung <syj91105@sookmyung.ac.kr>
2025-10-04 23:50:31 +0900

    [Silver I] Title: IOIOI, Time: 308 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Silver/5525. IOIOI/IOIOI.py
+++ Python/백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,3 +1,5 @@
+# 겹치는 IOI
+
 import sys
 input = sys.stdin.readline
 
@@ -5,12 +7,21 @@ N = int(input())
 M = int(input())
 S = input().rstrip()
 
-Pn = "IO" * N + "I"
-count = 0
+count = 0   # 연속된 IOI 개수
+result = 0  # Pn 완성 횟수
+i = 0
 
-for i in range(M):
-  if S[i] == 'I':
-    if S[i:i+(N * 2 + 1)] == Pn and i <= M:
-      count += 1
+while i < M - 1:
+    if S[i:i+3] == "IOI":
+        count += 1
+        # Pn 완성한 경우
+        if count == N:
+            result += 1
+            count -= 1  # IOI가 겹치기 때문에 하나 빼기
+        i += 2          # IOI를 찾았으니 다음 I로 이동
+    else:
+        count = 0
+        i += 1
 
-print(count)
+print(result)
+

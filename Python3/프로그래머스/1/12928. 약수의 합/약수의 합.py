994bc03f48204e5ba5861e00dbc1ca5f6e215757
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-04-21 00:33:32 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ Python3/프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,19 @@
 import math
 
+# 초기 답안 (시간 복잡도: O(n))
+# def solution(n):
+#     answer = 0
+#     for i in range(1, n+1):
+#         if n % i == 0:
+#             answer += i
+#     return answer
+
+# 시간 복잡도: O(√n)
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
+    for i in range(1, int(math.sqrt(n)) + 1):
         if n % i == 0:
             answer += i
+            if i != n // i:     # 같은 약수를 두 번 더하는 걸 막기 위한 조건
+                answer += n // i
     return answer

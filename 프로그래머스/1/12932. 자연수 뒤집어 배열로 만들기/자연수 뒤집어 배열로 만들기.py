a105aab537fc743561bc8e02fe6a676697d6d2a3
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-04-21 23:52:45 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,7 +1,8 @@
 def solution(n):
     answer = []
-    lst = list(str(n))
-    lst.reverse()
+    # lst = list(str(n))
+    # lst.reverse()
+    lst = str(n)[::-1]     # 문자열 슬라이싱으로 reverse 가능
     for i in lst:
         answer.append(int(i))
     return answer

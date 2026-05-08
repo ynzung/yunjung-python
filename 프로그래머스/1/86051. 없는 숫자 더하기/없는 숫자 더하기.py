2156d03b2c2287f074b8a4167524571856968912
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-05-08 15:25:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 8.94 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,8 +1,9 @@
 def solution(numbers):
-    answer = 0
-    for i in range(1, 10):
-        if i in numbers:
-            continue
-        else:
-            answer += i
+    # answer = 0
+    # for i in range(1, 10):
+    #     if i in numbers:
+    #         continue
+    #     else:
+    #         answer += i
+    answer = 45 - sum(numbers)
     return answer

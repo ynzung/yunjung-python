4ce5c7305d1ed502ae8c6ffcd653032701591bfa
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-05-12 00:56:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,14 @@
+def solution(num):
+    answer = 0
+    while num != 1:
+        if answer != 500:
+            if num % 2 == 0:
+                num //= 2
+                answer += 1
+            else:
+                num = 3 * num + 1
+                answer += 1
+        else: 
+            answer = -1
+            break
+    return answer

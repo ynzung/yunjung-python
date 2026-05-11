633e9ef41ccfaae84afafe2fa4001aaa206d5999
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-05-12 01:01:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,4 +1,5 @@
 def solution(phone_number):
     length = len(phone_number) - 4
     answer = '*' * length + phone_number[length:]
+    # answer = "*" * (len(phone_number) - 4) + phone_number[-4:]
     return answer

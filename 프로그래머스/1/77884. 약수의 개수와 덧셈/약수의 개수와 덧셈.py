c72a97bc6cd258dc54adceb64663ad2663253d6c
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-05-15 17:35:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 11.83 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -11,5 +11,19 @@ def solution(left, right):
             answer += i
         else:
             answer -= i
-
     return answer
+            
+'''            
+ i가 완전제곱수인지 확인하는 코드
+ 완전 제곱수는 약수의 개수가 홀수!!!!
+ 완전 제곱수가 아니면 약수의 개수가 짝수임
+'''
+
+# def solution(left, right):
+#     answer = 0
+#     for i in range(left,right+1):
+#         if int(i**0.5)==i**0.5:
+#             answer -= i
+#         else:
+#             answer += i
+#     return answer

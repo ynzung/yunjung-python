8f332e853f2e4b02534f06b0d59c0fce5e6ab93d
Yunjung Seong <syj91105@sookmyung.ac.kr>
2026-08-09 10:37:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,32 @@
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for word1, word2 in zip(arr1, arr2):
+        word1_binary = format(word1, f'0{n}b')
+        word2_binary = format(word2, f'0{n}b')
+        result = ''
+
+        for a, b in zip(word1_binary, word2_binary):
+            if a == '1' or b == '1':
+                result += '#'
+            else:
+                result += ' '
+
+        answer.append(result)
+
+    return answer
+
+# def solution(n, arr1, arr2):
+#     answer = []
+
+#     for num1, num2 in zip(arr1, arr2):
+
+#         # 비트 OR 연산: 둘 중 하나라도 1이면 1
+#         # OR 연산 결과를 길이 n의 이진수 문자열로 변환
+#         binary = format(num1 | num2, f'0{n}b')
+
+#         # 1은 벽('#'), 0은 공백으로 변환
+#         result = binary.replace('1', '#').replace('0', ' ')
+#         answer.append(result)
+
+#     return answer
